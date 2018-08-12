4932e5852aef744209746f9b70c2cf1cc166e500
chrisfon <39479488+chrisfon@users.noreply.github.com>
2018-08-12 16:10:40 -0600

    tarea5

--- Trabajos EXTRAclase/Tarea 12-8/Tarea5/src/CSVLogic.java
+++ Trabajos EXTRAclase/Tarea 12-8/Tarea5/src/CSVLogic.java
@@ -15,7 +15,7 @@ import javax.swing.JOptionPane;
 public class CSVLogic {
     //CAMBIAR PATH PARA QUE SIRVA EL PROGRAMA, SINO VA A TIRAR EXCEPCION
 
-    String csvFile = "PATH A CSV VA AQUI";
+    String csvFile = "E:\\Cosas del nuevo ssd\\Documents (setting de juegos y lol replays)\\NetBeansProjects\\Tarea5\\userlist.csv";
     BufferedReader br = null;
     String line = "";
     String SplitBy = ",";
